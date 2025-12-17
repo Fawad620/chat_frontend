206fc68ee41fe7a6bd2acb598ca9c61ed9daa1b4
Fawad620 <fawadaslam620@gmail.com>
2025-12-17 22:04:29 +0000

    update base url

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ export default function App() {
     setIsTyping(true);
     const API_URL = import.meta.env.VITE_API_URL;
     try {
-      const res = await fetch("https://backend-lc5h.vercel.app/api/chat", {
+      const res = await fetch("https://backend-gryl.vercel.app/api/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: userMsg.text }),
